654eb462c7cc3dc2204ba554fa27e2e903f45bad
Riccardo Biffi <biffi.riccardo91@gmail.com>
2024-12-16 01:29:47 +0100

    Add RPCs and utility functions

--- src/bitcoin/bitcoin-core.js
+++ src/bitcoin/bitcoin-core.js
@@ -14,15 +14,18 @@
 'use strict'
 
 const fetch = require('cross-fetch')
+const crypto = require('crypto')
+const bitcoin = require('bitcoinjs-lib');
 
 class BitcoinCore {
-  constructor (config = {}) {
+  constructor(config = {}) {
     this._network = config.network || 'regtest'
     this._defaultWallet = 'main.dat'
     this._node_uri = 'http://user:password@127.0.0.1:18443/wallet/' + this._defaultWallet || config.uri
   }
 
-  async init () {
+  async init() {
+    //await this.unloadAllWallets()
     await this.startWallet()
     const addr = await this.getNewAddress()
     if (addr.error) {
@@ -33,14 +36,76 @@ class BitcoinCore {
     this._miningAddress = addr.result
   }
 
-  async startWallet () {
+  async startWallet() {
     const createWallet = await this.createWallet()
     if (createWallet.error?.code === -4) {
       await this.loadWallet()
     }
   }
 
-  async _apiCall (method, params) {
+  createWallet(opts = {}) {
+    return this._apiCall('createwallet', [opts.filename || this._defaultWallet])
+  }
+
+  listWallets() {
+    return this._apiCall('listwallets', [])
+  }
+
+  listUnspent(opts = {}) {
+    return this._apiCall('listunspent', [opts.minconf || 1, opts.maxconf || 9999999, opts.addresses])
+  }
+
+  loadWallet(opts = {}) {
+    return this._apiCall('loadwallet', [opts.filename || this._defaultWallet])
+  }
+
+  unloadWallet(opts = {}) {
+    return this._apiCall('unloadwallet', [opts.filename || this._defaultWallet])
+  }
+
+  getBalance(opts) {
+    return this._apiCall('getbalance', opts)
+  }
+
+  getNewAddress(opts) {
+    return this._apiCall('getnewaddress', opts)
+  }
+
+  createRawTransaction(opts) {
+    return this._apiCall('createrawtransaction', [opts.inputs, opts.outputs, opts.locktime, opts.replaceable])
+  }
+
+  signRawTransactionWithWallet(opts) {
+    return this._apiCall('signrawtransactionwithwallet', [opts.hexstring, opts.prevtxs, opts.sighashtype || 'ALL'])
+  }
+
+  sendRawTransaction(opts) {
+    return this._apiCall('sendrawtransaction', [opts.hexstring, opts.maxfeerate || 0.1])
+  }
+
+  sendToAddress(opts) {
+    return this._apiCall('sendtoaddress', [opts.address, opts.amount])
+  }
+
+  async mine(opts = {}) {
+    const params = {
+      address: opts.address || this._miningAddress,
+      nBlocks: opts.blocks || 1
+    }
+
+    return this._apiCall('generatetoaddress', [params.nBlocks, params.address])
+  }
+
+  addressToScriptHash(address) {
+    const network = this._network === 'mainnet' ?
+      bitcoin.networks.bitcoin : bitcoin.networks.regtest
+    let script = bitcoin.address.toOutputScript(address, network)
+    let hash = bitcoin.crypto.sha256(script)
+    let reversedHash = hash.reverse()
+    return reversedHash.toString('hex')
+  }
+
+  async _apiCall(method, params) {
     const body = {
       jsonrpc: '1.0',
       id: `${method}_${(Math.random() * 10e20).toFixed()}`,
@@ -60,31 +125,6 @@ class BitcoinCore {
       return
     }
     return data
-  }
-
-  getNewAddress (opts) {
-    return this._apiCall('getnewaddress', opts)
-  }
-
-  createWallet (opts = {}) {
-    return this._apiCall('createwallet', [opts.filename || this._defaultWallet])
-  }
-
-  sendToAddress (opts) {
-    return this._apiCall('sendtoaddress', [opts.address, opts.amount])
-  }
-
-  loadWallet (opts = {}) {
-    return this._apiCall('loadwallet', [opts.filename || this._defaultWallet])
-  }
-
-  async mine (opts = {}) {
-    const params = {
-      address: opts.address || this._miningAddress,
-      nBlocks: opts.blocks || 1
-    }
-
-    return this._apiCall('generatetoaddress', [params.nBlocks, params.address])
   }
 }
 
